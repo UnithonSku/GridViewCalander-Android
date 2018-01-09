b1011e1046a786738b9de90e056d7c021a187dc7
geonyoungkim <c2619zz@naver.com>
2018-01-09 20:49:32 +0900

    m

---
+++ app/src/main/java/com/example/c2619/myapplication/DayInfo.java
@@ -0,0 +1,52 @@
+package com.example.c2619.myapplication;
+
+/**
+ * Created by c2619 on 2018-01-07.
+ */
+
+public class DayInfo
+{
+    private String day;
+    private boolean inMonth;
+
+    /**
+     * 날짜를 반환한다.
+     *
+     * @return day 날짜
+     */
+    public String getDay()
+    {
+        return day;
+    }
+
+    /**
+     * 날짜를 저장한다.
+     *
+     * @param day 날짜
+     */
+    public void setDay(String day)
+    {
+        this.day = day;
+    }
+
+    /**
+     * 이번달의 날짜인지 정보를 반환한다.
+     *
+     * @return inMonth(true/false)
+     */
+    public boolean isInMonth()
+    {
+        return inMonth;
+    }
+
+    /**
+     * 이번달의 날짜인지 정보를 저장한다.
+     *
+     * @param inMonth(true/false)
+     */
+    public void setInMonth(boolean inMonth)
+    {
+        this.inMonth = inMonth;
+    }
+
+}
